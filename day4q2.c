41b462cddc2871e6af1b72fcbd0aac62c161a323
Riddhi Mittal <riddhimittal13rm@gmail.com>
2026-02-05 11:46:00 +0530

    Add files via upload

---
+++ day4q2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, val, i, k = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter value to remove: ");
+    scanf("%d", &val);
+
+    for (i = 0; i < n; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    printf("k = %d\n", k);
+    printf("Array after removal:\n");
+    for (i = 0; i < k; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
