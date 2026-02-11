4d0265e32de3011573412e05781c01d262d18142
Riddhi Mittal <riddhimittal13rm@gmail.com>
2026-02-11 20:34:45 +0530

    Add files via upload

---
+++ day11q2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
