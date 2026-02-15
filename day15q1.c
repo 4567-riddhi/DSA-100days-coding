0007f7e23c8aaed1080b1a1fbd2a373a2c365508
Riddhi Mittal <riddhimittal13rm@gmail.com>
2026-02-15 14:32:57 +0530

    Add files via upload

---
+++ day15q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    
+    int matrix[m][n];
+    int sum = 0;
+    
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < m && i < n; i++) {
+        sum = sum + matrix[i][i];
+    }
+    
+    printf("Sum of primary diagonal = %d\n", sum);
+    
+    return 0;
+}
