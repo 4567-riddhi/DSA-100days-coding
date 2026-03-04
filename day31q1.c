54813688574c79db5d2207b559c4c92980d90d34
Riddhi Mittal <riddhimittal13rm@gmail.com>
2026-03-04 13:45:23 +0530

    Add files via upload

--- day31q1.c
+++ day31q1.c
@@ -1,62 +1,35 @@
 #include <stdio.h>
-
-#define MAX 100   // Maximum size of stack
-
-int stack[MAX];
-int top = -1;     // Initially stack is empty
-
-// Function to push element
-void push(int value) {
-    if (top == MAX - 1) {
-        printf("Stack Overflow\n");
-    } else {
-        top++;
-        stack[top] = value;
-    }
-}
-
-// Function to pop element
-void pop() {
-    if (top == -1) {
-        printf("Stack Underflow\n");
-    } else {
-        printf("%d\n", stack[top]);
-        top--;
-    }
-}
-
-// Function to display stack (top to bottom)
-void display() {
-    if (top == -1) {
-        printf("Stack is empty\n");
-    } else {
-        for (int i = top; i >= 0; i--) {
-            printf("%d ", stack[i]);
-        }
-        printf("\n");
-    }
-}
+#include <stdlib.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);   // number of operations
-
+    int n, m, val;
+ 
+    if (scanf("%d", &n) != 1) return 0;
+  
+    int stack[n];
+    int top = -1; 
+    
     for (int i = 0; i < n; i++) {
-        int choice, value;
-
-        scanf("%d", &choice);
-
-        if (choice == 1) {      // Push
-            scanf("%d", &value);
-            push(value);
-        } 
-        else if (choice == 2) { // Pop
-            pop();
-        } 
-        else if (choice == 3) { // Display
-            display();
+        if (scanf("%d", &val) == 1) {
+            stack[++top] = val; 
         }
     }
+
+    if (scanf("%d", &m) != 1) return 0;
+
+    for (int i = 0; i < m; i++) {
+        if (top >= 0) {
+            top--; 
+        }
+    }
+    
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+        if (i > 0) {
+            printf(" ");
+        }
+    }
+    printf("\n");
 
     return 0;
 }
